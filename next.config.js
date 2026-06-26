0c0455e4139ecf4970b0e72a145853a5a3f8cc2c
Saurabh Singh Yadav <saurabhssy07@gmail.com>
2026-06-27 00:04:35 +0530

    Fix Puppeteer Webpack bundling

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   experimental: {
-    serverComponentsExternalPackages: [],
+    serverComponentsExternalPackages: ['puppeteer-core', '@sparticuz/chromium'],
   },
   webpack: (config) => {
     config.resolve.alias.canvas = false;
